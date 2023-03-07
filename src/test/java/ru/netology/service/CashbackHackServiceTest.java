815a2128a135484bd01917d3f7064de3379a33e7
LiudmilaLobanova <liudmilanl@gmail.com>
2023-03-07 00:00:53 -0800

    Tests with tetsng added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,61 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemainToFirstCashBack() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int expected = 100;
+        int actual = service.remain(amount);
+
+        assertEquals(actual,expected);
+
+
+    }
+
+    @Test
+    public void testRemainToSecondCashBack() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1680;
+
+        int expected = 320;
+        int actual = service.remain(amount);
+
+        assertEquals(actual,expected);
+
+
+    }
+
+    @Test
+    public void testRemainIfNull() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+
+        int expected = 1000;
+        int actual = service.remain(amount);
+
+        assertEquals(actual,expected);
+
+
+    }
+
+    @Test
+    public void testRemainIfBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int expected = 0;
+        int actual = service.remain(amount);
+
+        assertEquals(actual,expected);
+
+
+    }
+
+}
